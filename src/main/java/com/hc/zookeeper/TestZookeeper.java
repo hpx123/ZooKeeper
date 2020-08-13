9f3d46219f117e55a54273524cfb9c4c9a51b50f
hpx123 <1983249619@qq.com>
2020-08-13 21:44:51 +0800

    today

--- src/main/java/com/hc/zookeeper/TestZookeeper.java
+++ src/main/java/com/hc/zookeeper/TestZookeeper.java
@@ -2,14 +2,16 @@ package com.hc.zookeeper;
 
 import org.apache.zookeeper.*;
 import org.apache.zookeeper.data.ACL;
+import org.apache.zookeeper.data.Stat;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.List;
 
 public class TestZookeeper {
 
-    private String connectString = "192.168,43.109:2181";
+    private String connectString = "127.0.0.1:2181";
     /**设置最大连接时间**/
     private int sessionTimeout = 10000;
     private ZooKeeper zkClient;
@@ -19,7 +21,18 @@ public class TestZookeeper {
         /**watcher是监听器**/
          zkClient = new ZooKeeper(connectString, sessionTimeout, new Watcher() {
             public void process(WatchedEvent watchedEvent) {
-
+                try {
+                    System.out.println("-------start----------");
+                    List<String> children = zkClient.getChildren("/", true);
+                    for(String child : children){
+                        System.out.println(child);
+                    }
+                    System.out.println("-------end---------");
+                } catch (KeeperException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
         });
     }
@@ -29,5 +42,29 @@ public class TestZookeeper {
     public void createNode() throws KeeperException, InterruptedException {
         String path = zkClient.create("/hututu", "huchengzuimei".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
         System.out.println(path);
+    }
+
+    /**获取子节点并监听节点的变化**/
+    @Test
+    public void getChildren(){
+        List<String> children = null;
+        try {
+            children = zkClient.getChildren("/", true);
+            for(String child : children){
+                System.out.println(child);
+            }
+            Thread.sleep(Long.MAX_VALUE);
+        } catch (KeeperException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**判断节点是否存在**/
+    @Test
+    public void exist() throws KeeperException, InterruptedException {
+        Stat stat = zkClient.exists("/hututu", false);
+        System.out.println(stat == null? "noexist" : "exist");
     }
 }
